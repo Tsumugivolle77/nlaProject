d3fecc5b60e1687d8121cc08567356365fc4c5eb
Tsumugivolle77 <endsieg77@gmail.com>
2025-01-12 19:11:15 +0100

    real mat to hessenberg implemented

--- README.md
+++ README.md
@@ -1,27 +1,44 @@
 Record dev. breakthroughs and my thoughts in this file.
 
-## Trivias
-To enable printing additional information:
+## Something unimportant
+To enable printing additional information or make the code more debug-friendly:
 
 `#define DEBUG` at the beginning of the `main.cpp`
 
 or
 
-`add_compile_option(-DDEBUG)` in `CMakeLists.txt`.
+`add_compile_option(-DDEBUG)` in `CMakeLists.txt`
+
+or 
+
+`g++ <blabla> -DDEBUG` in your terminal, etc.
 
 ## class `nla_mat`
 So I wrote a class to make my codes look better (more professional :D), hiding the details behind member functions.
 ```cpp
-template <typename Mat = arma::cx_mat>
-class nla_mat
-{
-private:
-    Mat mat;
+template<typename Mat = arma::cx_mat>
+class nla_mat {
+public:
     using elem_type = typename Mat::elem_type;
     using list      = std::initializer_list<elem_type>;
     using lists     = std::initializer_list<list>;
 
-    static Mat get_householder_mat(arma::Col<elem_type> x) {
+    nla_mat(Mat &&m);
+    nla_mat(lists &&li);
+
+    Mat       &get_mat();
+    const Mat &get_mat() const;
+    nla_mat to_hessenberg();
+
+#ifndef DEBUG
+private:
+#endif
+    Mat matx;
+
+    // for complex vector
+    template<typename ET>
+    static std::enable_if_t<std::is_same_v<ET, std::complex<double> >, arma::Mat<ET> >
+    get_householder_mat(arma::Col<ET> x) {
         using namespace arma;
         using namespace std::complex_literals;
 
@@ -32,46 +49,51 @@ private:
         const auto I = cx_mat(x.n_rows, x.n_rows, fill::eye);
 
         const cx_vec w = x + std::exp(1i * phase) * norm(x) * e1;
-        const cx_rowvec wh {w.ht()};
+        // const cx_vec w = x + norm(x) * e1;
+        const cx_rowvec wh{w.ht()};
 
         return I - 2 * w * wh / dot(wh, w);
     }
 
-public:
-    explicit nla_mat(Mat &&m): mat(m) { }
-
-    nla_mat(lists &&li): mat(li) { }
-
-    Mat       &get_mat()       { return mat; }
-    const Mat &get_mat() const { return mat; }
-
-    // get the Hessenberg form of mat
-    nla_mat to_hessenberg() {
+    // for real vector
+    template<typename ET>
+    static std::enable_if_t<!std::is_same_v<ET, std::complex<double> >, arma::Mat<ET> >
+    get_householder_mat(arma::Col<ET> x) {
         using namespace arma;
-        auto hess = mat;
+        using namespace std::complex_literals;
 
-        for (int i = 0; i < hess.n_cols - 1; ++i) {
-            auto x = cx_colvec(hess.submat(i + 1, i, hess.n_rows -1 , i));
-            auto H = get_householder_mat(x);
-            cx_mat Qi = cx_mat(hess.n_rows, hess.n_cols, fill::eye);
+        auto x1 = x[0];
+        auto e1 = colvec(x.n_rows);
+        e1[0] = 1.;
+        const auto I = mat(x.n_rows, x.n_rows, fill::eye);
 
-            Qi(span(i + 1, Qi.n_rows - 1), span(i + 1, Qi.n_cols - 1)) = H;
+        auto sgn = [](auto v) -> auto { return v >= 0 ? 1 : -1; };
+        const vec w = x + sgn(x1) * norm(x) * e1;
+        // const cx_vec w = x + norm(x) * e1;
+        const rowvec wh{w.t()};
 
-            hess = Qi * hess;
-
-#ifdef DEBUG
-            std::cout << "apply householder left\n" << mat << std::endl;
-#endif
-
-            hess = hess * Qi.ht();
-
-#ifdef DEBUG
-            std::cout << "apply householder right\n" << mat << std::endl;
-#endif
-        }
-
-        return hess;
+        return I - 2 * w * wh / dot(wh, w);
     }
-
 };
+```
+At the beginning of the class I defined alias for types and declared some member functions including constructors and so on. The implementation would be introduced above.
+
+Then I introduced a static member function `get_householder_mat` to compute the Householder Matrix from a given vector `x`. It has two overloads. For the complex vector it will call the first overload, otherwise the second will be called.
+
+I used `std::enable_if_t` for deciding which version to be called, which is based upon `SFINAE` in C++. But factually in this case, we can simply use function overload (they are equivalent this time). It anyway serves as a watermark for my own version :P.
+
+### Utility and member functions
+#### operator<<()
+This operator overload is for applying like `std::cout` or `fout` on our `nla_mat` object.
+```cpp
+// printing nla_mat
+template <typename Mat>
+std::ostream &operator<<(std::ostream &os, const nla_mat<Mat> &mat) {
+    return os << mat.get_mat();
+}
+```
+
+#### constructors
+```cpp
+
 ```
--- main.cpp
+++ main.cpp
@@ -8,16 +8,29 @@
 // or
 // `add_compile_option(-DDEBUG)` in CMakeLists.txt
 
-template <typename Mat = arma::cx_mat>
-class nla_mat
-{
-private:
-    Mat mat;
+template<typename Mat = arma::cx_mat>
+class nla_mat {
+public:
     using elem_type = typename Mat::elem_type;
     using list      = std::initializer_list<elem_type>;
     using lists     = std::initializer_list<list>;
 
-    static Mat get_householder_mat(arma::Col<elem_type> x) {
+    nla_mat(Mat &&m);
+    nla_mat(lists &&li);
+
+    Mat       &get_mat();
+    const Mat &get_mat() const;
+    nla_mat to_hessenberg();
+
+#ifndef DEBUG
+private:
+#endif
+    Mat matx;
+
+    // for complex vector
+    template<typename ET>
+    static std::enable_if_t<std::is_same_v<ET, std::complex<double> >, arma::Mat<ET> >
+    get_householder_mat(arma::Col<ET> x) {
         using namespace arma;
         using namespace std::complex_literals;
 
@@ -28,49 +41,81 @@ private:
         const auto I = cx_mat(x.n_rows, x.n_rows, fill::eye);
 
         const cx_vec w = x + std::exp(1i * phase) * norm(x) * e1;
-        const cx_rowvec wh {w.ht()};
+        // const cx_vec w = x + norm(x) * e1;
+        const cx_rowvec wh{w.ht()};
 
         return I - 2 * w * wh / dot(wh, w);
     }
 
-public:
-    explicit nla_mat(Mat &&m): mat(m) { }
-
-    nla_mat(lists &&li): mat(li) { }
-
-    Mat       &get_mat()       { return mat; }
-    const Mat &get_mat() const { return mat; }
-
-    // get the Hessenberg form of mat
-    nla_mat to_hessenberg() {
+    // for real vector
+    template<typename ET>
+    static std::enable_if_t<!std::is_same_v<ET, std::complex<double> >, arma::Mat<ET> >
+    get_householder_mat(arma::Col<ET> x) {
         using namespace arma;
-        auto hess = mat;
+        using namespace std::complex_literals;
 
-        for (int i = 0; i < hess.n_cols - 1; ++i) {
-            auto x = cx_colvec(hess.submat(i + 1, i, hess.n_rows -1 , i));
-            auto H = get_householder_mat(x);
-            cx_mat Qi = cx_mat(hess.n_rows, hess.n_cols, fill::eye);
+        auto x1 = x[0];
+        auto e1 = colvec(x.n_rows);
+        e1[0] = 1.;
+        const auto I = mat(x.n_rows, x.n_rows, fill::eye);
 
-            Qi(span(i + 1, Qi.n_rows - 1), span(i + 1, Qi.n_cols - 1)) = H;
+        auto sgn = [](auto v) -> auto { return v >= 0 ? 1 : -1; };
+        const vec w = x + sgn(x1) * norm(x) * e1;
+        // const cx_vec w = x + norm(x) * e1;
+        const rowvec wh{w.t()};
 
-            hess = Qi * hess;
-
-#ifdef DEBUG
-            std::cout << "apply householder left\n" << mat << std::endl;
-#endif
-
-            hess = hess * Qi.ht();
-
-#ifdef DEBUG
-            std::cout << "apply householder right\n" << mat << std::endl;
-#endif
-        }
-
-        return hess;
+        return I - 2 * w * wh / dot(wh, w);
     }
-
 };
 
+// printing nla_mat
+template <typename Mat>
+std::ostream &operator<<(std::ostream &os, const nla_mat<Mat> &mat) {
+    return os << mat.get_mat();
+}
+
+template<typename Mat>
+nla_mat<Mat>::nla_mat(Mat &&m): matx(m) {
+}
+
+template<typename Mat>
+nla_mat<Mat>::nla_mat(lists &&li): matx(li) {
+}
+
+template<typename Mat>
+Mat &nla_mat<Mat>::get_mat() { return matx; }
+
+template<typename Mat>
+const Mat &nla_mat<Mat>::get_mat() const { return matx; }
+
+// get the Hessenberg form of matx
+template<typename Mat>
+nla_mat<Mat> nla_mat<Mat>::to_hessenberg() {
+    using namespace arma;
+    auto hess = matx;
+
+    for (int i = 0; i < hess.n_cols - 2; ++i) {
+        auto x = Col<elem_type>(hess.submat(i + 1, i, hess.n_rows - 1, i));
+        auto H = get_householder_mat(x);
+        auto Qi = Mat(hess.n_rows, hess.n_cols, fill::eye);
+
+        Qi(span(i + 1, Qi.n_rows - 1), span(i + 1, Qi.n_cols - 1)) = H;
+
+        hess = Qi * hess;
+
+#ifdef DEBUG
+            std::cout << "apply householder left\n" << hess << std::endl;
+#endif
+
+        hess = hess * Qi.ht();
+
+#ifdef DEBUG
+            std::cout << "apply householder right\n" << hess << std::endl;
+#endif
+    }
+
+    return hess;
+}
 
 
 // arma::cx_mat QR_iteration(arma::cx_mat A, uint maxiter = 1000) {
@@ -84,20 +129,25 @@ public:
 //     return A;
 // }
 
-int main()
-{
+int main() {
     using namespace std::complex_literals;
     using namespace arma;
     nla_mat A = {
-        {arma::cx_double(4, 0), arma::cx_double(1, 2), arma::cx_double(3, -1), arma::cx_double(0, 1), arma::cx_double(2, -3)},
-        {arma::cx_double(1, -2), arma::cx_double(5, 0), arma::cx_double(1, 4), arma::cx_double(2, 1), arma::cx_double(0, -1)},
-        {arma::cx_double(3, 1), arma::cx_double(1, -4), arma::cx_double(6, 0), arma::cx_double(4, 2), arma::cx_double(1, 1)},
-        {arma::cx_double(0, -1), arma::cx_double(2, -1), arma::cx_double(4, -2), arma::cx_double(3, 0), arma::cx_double(5, -2)},
-        {arma::cx_double(2, 3), arma::cx_double(0, 1), arma::cx_double(1, -1), arma::cx_double(5, 2), arma::cx_double(8, 0)}
+        {cx_double(4, 0), cx_double(1, 2), cx_double(3, -1), cx_double(0, 1), cx_double(2, -3)},
+        {cx_double(1, -2), cx_double(5, 0), cx_double(1, 4), cx_double(2, 1), cx_double(0, -1)},
+        {cx_double(3, 1), cx_double(1, -4), cx_double(6, 0), cx_double(4, 2), cx_double(1, 1)},
+        {cx_double(0, -1), cx_double(2, -1), cx_double(4, -2), cx_double(3, 0), cx_double(5, -2)},
+        {cx_double(2, 3), cx_double(0, 1), cx_double(1, -1), cx_double(5, 2), cx_double(8, 0)}
     };
 
-    // A = QR_iteration(A);
-    std::cout << A.to_hessenberg() << std::endl;
+    nla_mat B = mat{
+        {1, 1, 4, 5},
+        {1, 4, 1, 9,},
+        {4, 1, 9, 8,},
+        {5, 9, 8, 0}
+    };
+
+    std::cout << A.to_hessenberg() << B.to_hessenberg() << std::endl;
 
     return 0;
 }
